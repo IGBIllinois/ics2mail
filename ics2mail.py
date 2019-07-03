a666ccab40804532c2d076ada8b161ad5ba1611d
Joe Leigh <jleigh@illinois.edu>
2019-07-03 11:39:22 -0500

    Added ability to handle multiple calendars

--- ics2mail.py
+++ ics2mail.py
@@ -6,41 +6,50 @@ from email.mime.text import MIMEText
 from icalevents.icalevents import events
 from pytz import timezone
 import config
+import sys
 
 # Calculate today and tomorrow
 now = datetime.datetime.now()
 tz = timezone('US/Central')
-today = datetime.datetime(
-    year=now.year,
-    month=now.month,
-    day=now.day,
-    tzinfo=tz
-)
+today = datetime.datetime(year=now.year, month=now.month, day=now.day, tzinfo=tz)
 tomorrow = today + datetime.timedelta(days=1)
 
 # Fetch the events
-events = events(url=config.cal_url, start=today, end=tomorrow)
+calendar_events = []
+events_to_print = False
+for calendar in config.calendars:
+    print("Fetching events from '{:}'...".format(calendar['title']))
+    tmp_events = events(url=calendar['url'], start=today, end=tomorrow, no_recurrence='no_recurrence' in calendar and calendar['no_recurrence'])
+    calendar_events.append(tmp_events)
+    if len(tmp_events) > 0:
+        events_to_print = True
 
 # Send the email
-if len(events) > 0:
+if events_to_print:
     msg = MIMEMultipart('alternative')
     msg['Subject'] = '{:s} - {:%A, %B %d, %Y}'.format(config.mail_subject, today)
     msg['From'] = config.mail_from
     msg['To'] = config.mail_to
     text_body = ""
     html_body = ""
-    for event in events:
-        if event.all_day:
-            text_body += config.all_day_text_template.format(event.summary, event.location, event.description)
-            html_body += config.all_day_html_template.format(event.summary, event.location, event.description)
-        else:
-            text_body += config.event_text_template.format(event.summary, event.start.astimezone(tz),
-                                                           event.end.astimezone(tz), event.location, event.description)
-            html_body += config.event_html_template.format(event.summary, event.start.astimezone(tz),
-                                                           event.end.astimezone(tz),
-                                                           event.location, event.description)
+    for i, calendar in enumerate(calendar_events):
+        text_body += config.header_text_template.format(config.calendars[i]['title'])
+        html_body += config.header_html_template.format(config.calendars[i]['title'])
+        for event in calendar:
+            if event.all_day:
+                text_body += config.all_day_text_template.format(event.summary, event.location, event.description)
+                html_body += config.all_day_html_template.format(event.summary, event.location, event.description)
+            else:
+                text_body += config.event_text_template.format(event.summary, event.start.astimezone(tz),
+                                                               event.end.astimezone(tz), event.location, event.description)
+                html_body += config.event_html_template.format(event.summary, event.start.astimezone(tz),
+                                                               event.end.astimezone(tz), event.location, event.description)
+        if len(calendar) == 0:
+            text_body += config.none_text_template
+            html_body += config.none_html_template
     text_body = config.text_template.format(text_body)
     html_body = config.html_template.format(html_body)
+
     part1 = MIMEText(text_body, 'plain')
     part2 = MIMEText(html_body, 'html')
     msg.attach(part1)
